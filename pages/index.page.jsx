bc13f25a69c219fbd59b6047be420260096cf381
wibunolife <khongbuoncuoi69@gmail.com>
2025-06-04 21:17:27 -0500

    m

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -7,4 +7,4 @@ export const Page = () => <NovelList />;
 export const documentProps = {
   title: 'Valvrareteam - Đọc Light Novel Vietsub Miễn Phí | Light Novel Tiếng Việt Hay Nhất',
   description: 'Thư viện Light Novel vietsub lớn nhất Việt Nam. Đọc Light Novel tiếng Việt miễn phí, cập nhật nhanh, dịch chất lượng cao. Hàng nghìn bộ Light Novel hay như Sword Art Online, Re:Zero, Overlord đang chờ bạn khám phá!'
-}; 
+};
